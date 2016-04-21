6b57d582c74c6033bc3b53ce22aa093bdfa75864
Ibrahim Mercado <Nazaboy19@Ibrahims-MacBook-Pro.local>
2016-04-21 18:18:01 -0400

    FinalEntry

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,31 @@
-game.js
+
+// Generate a random letter.
+letter = Math.round((Math.random() * 100))%100  + 1;
+
+
+counter = 9;
+do
+	{
+	counter++;
+
+	
+	document.write("Guess #"+counter+" was "+guess+"");
+
+	if (guess == letter) {
+		document.write("You guessed it!");
+		}
+	else  {
+		if (guess < letter)  {
+			document.write("Too small, guess again.");
+			}
+		else  {
+			document.write("Guess again.");
+			}
+		}
+
+		
+	} while (counter <9 && guess != letter);
+
+if (guess != letter) {
+		document.write("I won " + number+"");
+		}
